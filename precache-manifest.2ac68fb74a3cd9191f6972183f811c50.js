2377a1d7b2052ab2ec2221ed88394247a59bb566
Rajkumar180721 <rajkumar180721@gmail.com>
2020-10-01 20:11:46 +0530

    Updates

---
+++ precache-manifest.2ac68fb74a3cd9191f6972183f811c50.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b93fd03af663f34d09d3024798a7392",
+    "url": "/Rajkumar180721/archer.git/index.html"
+  },
+  {
+    "revision": "f9560ed6e5473132c7e2",
+    "url": "/Rajkumar180721/archer.git/static/css/main.d18101c0.chunk.css"
+  },
+  {
+    "revision": "15e3b2f4f12dd9366510",
+    "url": "/Rajkumar180721/archer.git/static/js/2.7f914a33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rajkumar180721/archer.git/static/js/2.7f914a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9560ed6e5473132c7e2",
+    "url": "/Rajkumar180721/archer.git/static/js/main.5ede2e58.chunk.js"
+  },
+  {
+    "revision": "c9d5a342b2ae14219b6f",
+    "url": "/Rajkumar180721/archer.git/static/js/runtime-main.e4064072.js"
+  },
+  {
+    "revision": "9e6336ffdfd76b26dcfac36e42cf5777",
+    "url": "/Rajkumar180721/archer.git/static/media/create_list.9e6336ff.png"
+  },
+  {
+    "revision": "2e14a851a516944373db1ef5aa4f728a",
+    "url": "/Rajkumar180721/archer.git/static/media/home.2e14a851.webp"
+  },
+  {
+    "revision": "e7e8eaff6b7ebe426b61432afe0a6bd5",
+    "url": "/Rajkumar180721/archer.git/static/media/like.e7e8eaff.webp"
+  },
+  {
+    "revision": "5787db407173ba5086c1146902ddcf4c",
+    "url": "/Rajkumar180721/archer.git/static/media/membersInQue.5787db40.png"
+  },
+  {
+    "revision": "30fd1d0d165c75f3d607de08c66a1036",
+    "url": "/Rajkumar180721/archer.git/static/media/music-cd.30fd1d0d.png"
+  },
+  {
+    "revision": "d9efb225926dec37a590ddd50a3e991f",
+    "url": "/Rajkumar180721/archer.git/static/media/mute.d9efb225.png"
+  },
+  {
+    "revision": "2a90a68d33e42a8bf7d3ce3873fc9402",
+    "url": "/Rajkumar180721/archer.git/static/media/que.2a90a68d.png"
+  },
+  {
+    "revision": "44bce19c26261417b392985ef46fe89e",
+    "url": "/Rajkumar180721/archer.git/static/media/search_icon.44bce19c.webp"
+  },
+  {
+    "revision": "e7403a65ff33d9823287bf1893a4278d",
+    "url": "/Rajkumar180721/archer.git/static/media/speaker-icon.e7403a65.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66866a3892a191e29742b2d3a8878a95.js"
+  "/Rajkumar180721/archer.git/precache-manifest.2ac68fb74a3cd9191f6972183f811c50.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rajkumar180721/archer.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
